f9d804f6a30f516700701bfe62f5471fffec4bf9
Tom <tomtooley2006@gmail.com>
2025-02-19 22:29:38 +0000

    first commit

---
+++ lua/plugins/neotree.lua
@@ -0,0 +1,20 @@
+return  { 
+    "nvim-neo-tree/neo-tree.nvim",
+    branch = "v3.x",
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+      "nvim-tree/nvim-web-devicons",
+      "MunifTanjim/nui.nvim" 
+    },
+    config = function()
+      require('neo-tree').setup {
+        filesystem = {
+          filtered_items = {
+            visible = true,
+            hide_dotfiles = false,
+            hide_gitignored = true
+    }}}
+      vim.keymap.set('n', '<C-n>', ':Neotree filesystem reveal left toggle<CR>')
+    end
+}
+
